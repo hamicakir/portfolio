163780e32909a6197f40ee933cefb0f820ee1a83
hamicakir <hmi.cakir@gmail.com>
2017-02-10 16:08:40 +0300

    Bootstrap import

---
+++ header.php
@@ -0,0 +1,48 @@
+<?php  include_once('database.php'); ?>
+<html lang="tr-TR">
+<head>
+    <title>Hami ÇAKIR Personal Webpage</title>
+    <meta charset="utf-8"/>
+    <meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1"/>
+    <link rel="canonical" href="http://www.hamicakir.com"/>
+    <link rel='shortlink' href="http://www.hamicakir.com"/>
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css"/>
+    <link rel="stylesheet" type="text/css" href="css/iconfonts.css"/>
+    <link rel="stylesheet" type="text/css" href="css/style.css"/>
+</head>
+<body>
+<div class="bg-image">
+    <img src="images/bg-image.jpg" alt="" />
+</div>
+<div class="main">
+    <div>
+        <header>
+            <nav>
+                <div class="navbar navbar-brand">
+                    <img class="logo" src="images/logo.png"/>
+                </div>
+                <div class="navbar-right">
+                    <ul class="nav navbar-nav menu">
+                        <li>
+                            <a href="#about-me-section">About Me</a>
+                        </li>
+                        <li>
+                            <a href="#portfolio-section">Biogrophy</a>
+                        </li>
+                        <li>
+                            <a href="#skills-section">Skills</a>
+                        </li>
+                        <li>
+                            <a href="blog.php">Blog</a>
+                        </li>
+                        <li>
+                            <a href="#contact-me">Contact</a>
+                        </li>
+                    </ul>
+                </div>
+
+            </nav>
+        </header>
+
+    </div>
+
